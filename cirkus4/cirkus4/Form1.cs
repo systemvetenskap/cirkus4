4b127bf57cb8c4c1342e991e4537a69b80db67fb
Martin <sq.martin91@gmail.com>
2015-09-07 10:11:15 +0300

    test 2.0

--- cirkus4/cirkus4/Form1.cs
+++ cirkus4/cirkus4/Form1.cs
@@ -19,7 +19,7 @@ namespace cirkus4
 
         private void button1_Click(object sender, EventArgs e)
         {
-            //Push?!
+            //Push?! VART HAMNA DETTA?!?!?
         }
     }
 }
